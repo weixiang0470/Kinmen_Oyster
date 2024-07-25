f015fe4ee2948c3fd4e1acc55d7a2f1fbb9739c4
samlin911227 <samlin911227@gmail.com>
2024-07-25 17:25:19 +0800

    V1

---
+++ main.js
@@ -0,0 +1,8 @@
+function ShowPick() {
+    var modal = document.getElementById('id01');
+    modal.style.display = 'block';
+}
+function ShowPeel(){
+    var modal = document.getElementById('id02');
+    modal.style.display = 'block';
+}
